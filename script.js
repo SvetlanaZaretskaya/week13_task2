644ac0cb3b6db88aac65ae06cde4d4e7102584fb
SvetlanaZaretskaya <svetlanazaretskaya@yandex.ru>
2024-09-22 21:26:34 +0300

    updates

--- script.js
+++ script.js
@@ -11,18 +11,24 @@ buttonClick.addEventListener("click", function() {
     let differenceInDays = Math.floor(difference/1000/60/60/24);
     let paragraph = document.getElementById("result");
 
+    let inputClick = document.getElementById('start');
+    inputClick.addEventListener("click", function() {
+        paragraph.textContent = "";
+    })
+
     if (birthdayDate == "") {
         paragraphError.textContent = `Ошибка, введите дату`
     }
 
 
-    if (differenceInDays.toString().endsWith('1')) 
+    if (differenceInDays.toString().endsWith('1') && !(differenceInDays.toString().endsWith('11'))) 
         {
         paragraph.textContent = `Остался ${differenceInDays} день до вашего дня рождения`;
     } 
-    else if (differenceInDays.toString().endsWith('2') || 
-    differenceInDays.toString().endsWith('3') || 
-    differenceInDays.toString().endsWith('4')) 
+    else if (differenceInDays.toString().endsWith('2') && !(differenceInDays.toString().endsWith('12')) || 
+    differenceInDays.toString().endsWith('4')  && !(differenceInDays.toString().endsWith('14'))|| 
+    differenceInDays.toString().endsWith('3')   && !(differenceInDays.toString().endsWith('13')) 
+) 
     {
         paragraph.textContent = `Осталось ${differenceInDays} дня до вашего дня рождения`;
     }
@@ -32,7 +38,11 @@ buttonClick.addEventListener("click", function() {
             differenceInDays.toString().endsWith('7')  ||
             differenceInDays.toString().endsWith('8')  ||
             differenceInDays.toString().endsWith('9')  ||
-            differenceInDays.toString().endsWith('0')
+            differenceInDays.toString().endsWith('0')  ||
+            differenceInDays.toString().endsWith('11') ||
+            differenceInDays.toString().endsWith('12') ||
+            differenceInDays.toString().endsWith('13') ||
+            differenceInDays.toString().endsWith('14')
              
     )
     {
